0b5c31d6e7e5c7f72a4fa7d4ddd9eefa6e9a7c0f
Sharjeel Sohail <sharjeelsoh@gmail.com>
2023-01-26 16:52:58 +1100

    components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import "./style.scss";
+
 function App() {
   return (
-    <div>Hello World!</div>
+    <Home />
   );
 }
 
